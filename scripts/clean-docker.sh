c706c3a85e85b56c12783be873f3927be4906426
Bernard Maltais <bernard.maltais@canada.ca>
2021-04-27 23:31:12 +0000

    Adding script to cleanup docker system

---
+++ scripts/clean-docker.sh
@@ -0,0 +1,6 @@
+sudo docker system prune -a --volume
+#sudo docker container prune --filter "until=2h"
+#sudo docker container prune -f
+#sudo docker image prune -f
+#sudo docker network prune -f
+#sudo docker volume prune -f
