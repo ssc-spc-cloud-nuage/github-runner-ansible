62664b9d9512e970de22f22c59a1bb2f56439c49
Bernard Maltais <bernard.maltais@canada.ca>
2021-09-17 23:27:22 +0000

    Update kubectl

--- roles/install_tools/files/installers/kubernetes-tools.sh
+++ roles/install_tools/files/installers/kubernetes-tools.sh
@@ -8,10 +8,8 @@
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 touch /etc/apt/sources.list.d/kubernetes.list
 
-# Based on https://kubernetes.io/docs/tasks/tools/install-kubectl/, package is xenial for both OS versions.
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
-apt-get update
-apt-get install -y kubectl
+sudo curl -sSL -o /usr/local/bin/kubectl "https://dl.k8s.io/release/v1.21.0/bin/linux/amd64/kubectl"
+sudo chmod +x /usr/local/bin/kubectl
 
 # Install Helm
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
